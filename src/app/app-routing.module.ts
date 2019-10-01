bb40aaf8e25e552ac07221d3a3b55e1752c4f61e
Ordash <farkas.bako@gmail.com>
2019-10-01 23:48:46 +0200

    Task-list component implemented, tasks are displayed in a simple table with limited fields

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
+import { TaskListComponent } from './task-list/task-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'tasks', pathMatch: 'full'},
+  { path: 'tasks', component: TaskListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/task-list/task-list.component.ts
@@ -0,0 +1,22 @@
+import { TaskService } from './../service/task.service';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../model/user';
+import { Task } from '../model/task';
+
+@Component({
+  selector: 'app-task-list',
+  templateUrl: './task-list.component.html',
+  styleUrls: ['./task-list.component.css']
+})
+export class TaskListComponent implements OnInit {
+
+  tasks: Task[];
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit() {
+    this.taskService.findAll().subscribe(data => {
+      this.tasks = data;
+    });
+  }
+}
